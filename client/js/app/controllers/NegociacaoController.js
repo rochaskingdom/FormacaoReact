20dc0e82307264e762020c7ea723f4b45b25c599
Vinicius Rocha <viniciusr.rocha@outlook.com>
2019-12-05 09:55:57 -0300

    Arrow Functions: deixando o código ainda menos verboso

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -16,15 +16,18 @@ class NegociacaoController {
 
         let data = new Date(...
             this._inputData.value
-            .split('-')
-            .map(function(item, indice) {
-                
-                return item - indice % 2;
-            })
-            
+            .split('-') 
+            .map((item, indice) => item - indice % 2)  /*.map(function(item, indice) {
+                                                        return item - indice % 2;
+                                                        })*/
         );
 
-        console.log(data);
+        let negociacao = new Negociacao(
+
+            data,
+            this._inputQuantidade.value,
+            this._inputValor.value
+        );
         
     }
 }
