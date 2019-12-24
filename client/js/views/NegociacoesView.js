d970f490dfe42972b995bf3dc6196d89acf5b62d
Vinicius Rocha <viniciusr.rocha@outlook.com>
2019-12-24 08:47:41 -0300

    Enxugando o código

--- client/js/views/NegociacoesView.js
+++ client/js/views/NegociacoesView.js
@@ -18,17 +18,16 @@ class NegociacoesView {
             </thead>
 
         <tbody>
-            ${model.negociacoes.map((n) => {
-                
-                return `
+            ${model.negociacoes.map(n => `
+            
                     <tr>
                         <td>${DateHelper.dataParaTexto(n.data)}</td>
                         <td>${n.quantidade}</td>
                         <td>${n.valor}</td>
                         <td>${n.volume}</td>
                     </tr>
-                `
-            }).join('')}
+
+                `).join('')}
         </tbody>
 
         <tfoot>
