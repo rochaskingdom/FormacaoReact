842edf01090ccd0f395682a138601820aedc5164
Vinicius Rocha <viniciusr.rocha@outlook.com>
2019-12-24 10:09:14 -0300

    Criando o método apaga

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -32,6 +32,15 @@ class NegociacaoController {
         
     }
 
+    apaga() {
+
+        this._listaNegociacoes.esvazia();
+        this._negociacoesView.update(this._listaNegociacoes);
+
+        this._mensagem.texto = 'Negociações apagadas com sucesso';
+        this._mensagemView.update(this._mensagem);
+    }
+
     _criaNegociacao() {
 
         return new Negociacao(
