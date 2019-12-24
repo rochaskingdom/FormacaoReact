76c61d1b7587ec914a9a7f537c400892fbdd387a
Vinicius Rocha <viniciusr.rocha@outlook.com>
2019-12-24 09:48:45 -0300

    Construtor vs super

--- client/js/app/views/View.js
+++ client/js/app/views/View.js
@@ -5,8 +5,13 @@ class View {
         this._elemento = elemento;
     }
 
+    template() {
+
+        throw new Error('O método template deve ser implementado');
+    }
+
     update(model) {
 
-        this._elemento.innerHTML = this._template(model);
+        this._elemento.innerHTML = this.template(model);
     }
 }
