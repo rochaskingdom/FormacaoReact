e282ea32576fa63a898ebd8feb277c7fbb23f51a
Vinicius Rocha <viniciusr.rocha@outlook.com>
2019-12-24 09:42:24 -0300

    Herança e reutilização de código

--- client/js/app/views/MensagemView.js
+++ client/js/app/views/MensagemView.js
@@ -1,8 +1,7 @@
-class MensagemView {
+class MensagemView extends View {
 
     constructor(elemento) {
-
-        this._elemento = elemento;
+        super(elemento);
     }
 
     _template(model) {
@@ -10,8 +9,4 @@ class MensagemView {
         return model.texto ? `<p class="alert alert-info">${model.texto}</p>` : '<p></p>';
     }
 
-    update(model) {
-        
-        this._elemento.innerHTML = this._template(model);
-    }
 }
--- client/js/app/views/NegociacoesView.js
+++ client/js/app/views/NegociacoesView.js
@@ -1,7 +1,7 @@
-class NegociacoesView {
+class NegociacoesView extends View {
 
     constructor(elemento) {
-        this._elemento = elemento;
+        super(elemento);
     }
 
     _template(model) {
@@ -38,10 +38,5 @@ class NegociacoesView {
         </tfoot>
     </table>     
     `;
-    }
-
-    update(model) {
-
-        this._elemento.innerHTML = this._template(model);
     }
 }
