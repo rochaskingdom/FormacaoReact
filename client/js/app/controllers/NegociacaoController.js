5fbfc50dea617c43fcb494fcc2124837b677ba1a
Vinicius Rocha <viniciusr.rocha@outlook.com>
2019-12-30 08:24:39 -0300

    Requisições Ajax com o objeto XMLHttpRequest

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -25,6 +25,30 @@ class NegociacaoController {
         this._mensagem.texto = 'Negociação adicionada com sucesso'; 
         this._limpaFormulario();   
     }
+
+    importaNegociacoes() {
+
+        let xhr = new XMLHttpRequest();
+
+        xhr.open('GET', 'negociacoes/semana');
+
+        xhr.onreadystatechange = () => {
+
+            if(xhr.readyState == 4) {
+
+                if(xhr.status == 200) {
+                    
+                    console.log('Obtendo as negociações do servidor.');
+                    
+                } else {
+                    console.log('Não foi possível obter as negociações do servidor.');
+                }
+            }
+            
+        };
+
+        xhr.send();        
+    }
     
     apaga() {
         
